00935db14c00cd492d2e451086cc89c94d9fbeca
Henry Ell <henryell87@gmail.com>
2019-07-09 09:44:37 -0500

    Add files via upload

---
+++ CalculatorApplication/CalculatorApplication/NumbrManipulation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication
+{
+    class NumbrManipulation
+    {
+        public int FindMax(int num1, int num2)
+        {
+            /*initial result */
+            int result;
+            if (num1 > num2)
+                result = num1;
+            else
+                result = num2;
+            return result;
+        }
+        static void Main(string[] args)
+        { 
+            /*loca variable definition*/
+            int a = 100;
+            int b = 99;
+            int ret;
+        NumbrManipulation n = new NumbrManipulation();
+            ret = n.FindMax(a, b);
+            Console.WriteLine("Maximum Number is: {0}", ret);
+            Console.ReadLine();
+
+        }
+    }
+}
